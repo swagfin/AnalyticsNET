b9765b5d0dc51abf9633adbabb2fe21a8b98efd1
swagfin <georgewainaina18@gmail.com>
2021-09-12 18:35:54 +0300

    Changed from Timer to Thread

--- AnalyticsNET/AnalyticsService.cs
+++ AnalyticsNET/AnalyticsService.cs
@@ -2,19 +2,21 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Timers;
+using System.Threading;
 
 namespace AnalyticsNET.Services
 {
     /// <summary>
     /// Main analytics service, This service should be declared as s Singleton (Single Instance)
     /// </summary>
-    public class AnalyticsService
+    public class AnalyticsService : IDisposable
     {
         private AnalyticConfiguration configurations { get; set; }
 
         private AnalyticsDeviceOptions AnalyticsDeviceOptions { get; set; }
         private readonly IAnalyticsLogger logger;
+        private Thread _analyticThread;
+
         private ConcurrentQueue<Trait> pendingToSendTraits { get; set; } = new ConcurrentQueue<Trait>();
 
         private string _statusRead { get; set; } = "Stopped";
@@ -23,7 +25,6 @@ namespace AnalyticsNET.Services
             get { return _statusRead; }
             set { _statusRead = value; }
         }
-        private Timer analyticsTimer { get; set; }
 
 
         public AnalyticsService(AnalyticsDeviceOptions analyticsDeviceOptions, IAnalyticsLogger analyticsLogger = null)
@@ -57,12 +58,8 @@ namespace AnalyticsNET.Services
                     AdditionalRequestParameters = new Dictionary<string, string>(),
                     NextCallBackInMilliseconds = 3000
                 };
-
                 //Create an Instance of Timer
-                analyticsTimer = new Timer();
-                analyticsTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
-                analyticsTimer.Interval = this.configurations.NextCallBackInMilliseconds;
-                analyticsTimer.Enabled = true;
+                StartAnalyticBackgroundThread();
             }
             catch (Exception ex)
             {
@@ -75,13 +72,16 @@ namespace AnalyticsNET.Services
         /// </summary>
         public void StopService()
         {
-            logger.LogInformation("Stopping Analytics Service");
-            this.analyticsStatus = AnalyticsStatus.Stopping.ToString();
-            if (analyticsTimer != null)
-                analyticsTimer.Enabled = false;
-            analyticsTimer = null;
-            this.analyticsStatus = AnalyticsStatus.Stopped.ToString();
-            //Unprocessed
+            try
+            {
+                logger.LogInformation("Stopping Analytics Service");
+                this.analyticsStatus = AnalyticsStatus.Stopping.ToString();
+                this._analyticThread.Abort();
+                this.analyticsStatus = AnalyticsStatus.Stopped.ToString();
+                //Unprocessed
+                logger.LogInformation("Stopped Analytics Service");
+            }
+            catch { }
         }
 
         public string GetAnalyticsStatus() => this.analyticsStatus;
@@ -95,96 +95,102 @@ namespace AnalyticsNET.Services
                 this.Track(trait);
         }
 
-        private async void OnTimedEvent(object source, ElapsedEventArgs e)
+        private void StartAnalyticBackgroundThread()
         {
-            analyticsTimer.Stop();
+            //Set Status
             this.analyticsStatus = AnalyticsStatus.Running.ToString();
-            //Track Last Seen
-            if (this.AnalyticsDeviceOptions.TrackDeviceHeartBeat)
-                this.Track(new Trait { Key = "heartBeat", Value = DateTime.Now.ToString() });
-            //Proceed
-            var readyToSendTraits = this.pendingToSendTraits.Where(x => x.SentSuccesfully != true && x.NextSending < DateTime.Now).ToList().Take(20);
-            //Check if Null
-            if (readyToSendTraits != null && readyToSendTraits.Count() > 0)
+            this._analyticThread = new Thread(async () =>
             {
-
-                logger.LogInformation($"Executing ({readyToSendTraits.Count()}) Analytics");
-                //Append All Traits
-                string allTraitsAppend = string.Empty;
-                foreach (Trait trait in readyToSendTraits)
-                    allTraitsAppend = $"{allTraitsAppend}{trait.Key} : {trait.Value}{Environment.NewLine}";
-
-                try
+                while (this.analyticsStatus == AnalyticsStatus.Running.ToString() || this.analyticsStatus == AnalyticsStatus.Seeding.ToString())
                 {
-                    logger.LogInformation($"Sending All Traits");
-                    this.analyticsStatus = AnalyticsStatus.Seeding.ToString();
-                    //Parameters
-                    Dictionary<string, string> parameters = new Dictionary<string, string>();
-                    parameters.Add("traitKey", "rawData");
-                    parameters.Add("traitValue", allTraitsAppend);
-                    //Add Configs
-                    parameters.Add("appName", this.AnalyticsDeviceOptions.AppName);
-                    parameters.Add("deviceName", this.AnalyticsDeviceOptions.DeviceName);
-                    parameters.Add("deviceId", this.AnalyticsDeviceOptions.DeviceID);
-                    parameters.Add("sessionId", this.AnalyticsDeviceOptions.CurrentAnalyticTrackingId);
-                    //Add Default
-                    foreach (var param in configurations.AdditionalRequestParameters)
-                        parameters.Add(param.Key, param.Value);
 
-                    //Headers
-                    Dictionary<string, string> headers = new Dictionary<string, string>();
-                    headers.Add("appSecret", this.AnalyticsDeviceOptions.AppSecretKey);
-
-                    //Add Default
-                    foreach (var header in configurations.AdditionalRequestHeaders)
-                        headers.Add(header.Key, header.Value);
-                    //Call Http Client
-                    string response = await CoreLib.RequestAsync(configurations.EndPointUrl, System.Net.Http.HttpMethod.Post, parameters, headers);
                     this.analyticsStatus = AnalyticsStatus.Running.ToString();
-                    //Update
-                    foreach (Trait trait in readyToSendTraits)
+                    //Track Last Seen
+                    if (this.AnalyticsDeviceOptions.TrackDeviceHeartBeat)
+                        this.Track(new Trait { Key = "heartBeat", Value = DateTime.Now.ToString() });
+                    //Proceed
+                    var readyToSendTraits = this.pendingToSendTraits.Where(x => x.SentSuccesfully != true && x.NextSending < DateTime.Now).ToList().Take(100);
+                    //Check if Null
+                    if (readyToSendTraits.Any())
                     {
-                        trait.SentSuccesfully = true;
-                        trait.FailedCount = 0;
-                        logger.LogInformation($"Trait: {trait.Id} Sent Successfully, Response: {response}");
-                        RemoveTraitFromQueue(trait);
+                        logger.LogInformation($"Dispatching Analytics");
+                        //Append All Traits
+                        string allTraitsAppend = string.Empty;
+                        foreach (Trait trait in readyToSendTraits)
+                            allTraitsAppend = $"{allTraitsAppend}{trait.Key} : {trait.Value}{Environment.NewLine}";
+
+                        try
+                        {
+                            this.analyticsStatus = AnalyticsStatus.Seeding.ToString();
+                            //Parameters
+                            Dictionary<string, string> parameters = new Dictionary<string, string>();
+                            parameters.Add("traitKey", "rawData");
+                            parameters.Add("traitValue", allTraitsAppend);
+                            //Add Configs
+                            parameters.Add("appName", this.AnalyticsDeviceOptions.AppName);
+                            parameters.Add("deviceName", this.AnalyticsDeviceOptions.DeviceName);
+                            parameters.Add("deviceId", this.AnalyticsDeviceOptions.DeviceID);
+                            parameters.Add("sessionId", this.AnalyticsDeviceOptions.CurrentAnalyticTrackingId);
+                            //Add Default
+                            foreach (var param in configurations.AdditionalRequestParameters)
+                                parameters.Add(param.Key, param.Value);
+                            //Headers
+                            Dictionary<string, string> headers = new Dictionary<string, string>();
+                            headers.Add("appSecret", this.AnalyticsDeviceOptions.AppSecretKey);
+                            //Add Default
+                            foreach (var header in configurations.AdditionalRequestHeaders)
+                                headers.Add(header.Key, header.Value);
+                            //Call Http Client
+                            string response = await CoreLib.RequestAsync(configurations.EndPointUrl, System.Net.Http.HttpMethod.Post, parameters, headers);
+                            this.analyticsStatus = AnalyticsStatus.Running.ToString();
+                            //Update
+                            int sentSuccess = 0;
+                            foreach (Trait trait in readyToSendTraits)
+                            {
+                                trait.SentSuccesfully = true;
+                                trait.FailedCount = 0;
+                                sentSuccess++;
+                                Trait _key = trait;
+                                this.pendingToSendTraits.TryDequeue(out _key);
+                            }
+                            if (sentSuccess > 0)
+                                logger.LogInformation($"Successfully dispatched ({sentSuccess}) Traits, Response: {response}");
+
+                            //Set Next Call Back
+                            SetNextCallBackFromServerResponse(response);
+                            //Detected One has been Sent try to Reque
+                            RequeueAllUnSent();
+                        }
+                        catch (Exception ex)
+                        {
+                            foreach (Trait trait in readyToSendTraits)
+                            {
+                                logger.LogError($"Error Sending Trait: {trait.Id}, Exception: {ex.Message}");
+                                //Mark to be Requeued
+                                trait.FailedCount++;
+                                trait.NextSending = trait.NextSending.AddMinutes(trait.FailedCount);
+                                logger.LogInformation($"Added Trait: {trait.Id} to Queue To Send Later at: {trait.NextSending}");
+                            }
+                            //Maximum Fails to Stop
+                            int maxFailed = GetFailedTraitsCount();
+                            if (maxFailed >= this.AnalyticsDeviceOptions.MaxFailedToAbort)
+                                OnMaxFailedToSendTraitsReached(maxFailed);
+                        }
+
                     }
 
-                    //Set Next Call Back
-                    SetNextCallBackFromServerResponse(response);
-                    //Detected One has been Sent try to Reque
-                    RequeueAllUnSent();
 
+                    //Sleep Thread
+                    Thread.Sleep((this.configurations.NextCallBackInMilliseconds) < 3000 ? 3000 : this.configurations.NextCallBackInMilliseconds);
                 }
-                catch (Exception ex)
-                {
-                    foreach (Trait trait in readyToSendTraits)
-                    {
-                        logger.LogError($"Error Sending Trait: {trait.Id}, Exception: {ex.Message}");
-                        //Mark to be Requeued
-                        trait.FailedCount++;
-                        trait.NextSending = trait.NextSending.AddMinutes(trait.FailedCount);
-                        logger.LogInformation($"Added Trait: {trait.Id} to Queue To Send Later at: {trait.NextSending}");
-                    }
-
-                    //Maximum Fails to Stop
-                    int maxFailed = GetFailedTraitsCount();
-                    if (maxFailed >= this.AnalyticsDeviceOptions.MaxFailedToAbort)
-                        OnMaxFailedToSendTraitsReached(maxFailed);
-                }
-
-            }
-            //RENABLE THE CLOCK
-            if (analyticsTimer != null)
-                analyticsTimer.Start();
+            });
+            this._analyticThread.Start();
         }
 
         private void SetNextCallBackFromServerResponse(string response)
         {
             try
             {
-                logger.LogInformation($"Attempting to Get Session ID and Next CallBack from Response");
-
                 var dataX = response.Split('|');
                 int nextCallback = 0;
                 //Check
@@ -193,16 +199,14 @@ namespace AnalyticsNET.Services
 
                 int.TryParse(dataX[0], out nextCallback);
                 //Check
-                if (nextCallback < 1000)
-                    nextCallback = 1000;
+                if (nextCallback < 3000)
+                    nextCallback = 3000;
                 //Check if CallBack Changed
                 if (nextCallback != this.configurations.NextCallBackInMilliseconds)
                 {
                     this.configurations.NextCallBackInMilliseconds = nextCallback;
-                    analyticsTimer.Interval = this.configurations.NextCallBackInMilliseconds;
                     logger.LogInformation($"CallBack Changed To: {nextCallback}");
                 }
-
                 //Get Session ID
                 string nextSessionId = string.Empty;
                 nextSessionId = dataX[1];
@@ -220,28 +224,16 @@ namespace AnalyticsNET.Services
             }
         }
 
-        public string GetCurrentSessionTrackingID() => this.AnalyticsDeviceOptions.CurrentAnalyticTrackingId;
 
         protected virtual void OnMaxFailedToSendTraitsReached(int failedCount)
         {
             logger.LogInformation($"Maximum Failed Traits ({failedCount}) reached, Terminating Analytics service");
             StopService();
         }
-        private void RemoveTraitFromQueue(Trait trait)
-        {
-            logger.LogInformation($"Attempting to Remove Trait: {trait.Id}");
-            //Remove Trait
-            bool removed = this.pendingToSendTraits.TryDequeue(out trait);
-            if (removed)
-                logger.LogInformation($"Trait: {trait.Id} REMOVED from Queue");
-            else
-                logger.LogWarning($"Trait: {trait.Id} WAS NOT REMOVED from Queue");
-        }
-
         private void RequeueAllUnSent()
         {
             var allUnsent = this.pendingToSendTraits.Where(x => x.SentSuccesfully != true && x.FailedCount > 0);
-            if (allUnsent != null && allUnsent.Count() > 0)
+            if (allUnsent.Any())
             {
                 logger.LogInformation($"Requeing ({allUnsent.Count()}) Analytics");
                 foreach (Trait trait in allUnsent)
@@ -252,6 +244,10 @@ namespace AnalyticsNET.Services
             }
         }
 
+        public string GetCurrentSessionTrackingID() => this.AnalyticsDeviceOptions.CurrentAnalyticTrackingId;
         public int GetFailedTraitsCount() => this.pendingToSendTraits.Where(x => x.SentSuccesfully != true && x.FailedCount > 0).Count();
+
+        public void Dispose() => StopService();
+
     }
 }

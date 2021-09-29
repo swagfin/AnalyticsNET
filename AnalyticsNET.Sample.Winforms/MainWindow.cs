60452468401ace33ec47b38da46a82f01dc91af5
George Wainaina Njeri <george@pixsel.net>
2021-09-29 07:58:55 +0300

    Minor Code refactoring

--- AnalyticsNET.Sample.Winforms/MainWindow.cs
+++ AnalyticsNET.Sample.Winforms/MainWindow.cs
@@ -25,7 +25,7 @@ namespace AnalyticsNET.Sample.Winforms
             this._analyticService = new AnalyticsService(new AnalyticsDeviceOptions
             {
                 AppName = "SEMANTICPOS",
-                AppSecretKey = "someHashHashKey125456",
+                AppSecretKey = "someHashHashKey12545678",
                 DeviceID = "EMUD-A001-B001-C001-D001",
                 DeviceName = Environment.MachineName,
                 TrackDeviceHeartBeat = true,
